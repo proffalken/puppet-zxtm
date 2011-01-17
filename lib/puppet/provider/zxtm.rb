9975c707057e20afe30ea46618c61c52f1046b6f
proffalken <theprofessor@threedrunkensysadsonthe.net>
2011-01-17 20:45:42 +0000

    added comments

--- lib/puppet/provider/zxtm.rb
+++
@@ -1,2 +0,0 @@
-require 'rubygems'
-require 'zxtm-api'
---
+++ lib/puppet/type/zxtm.rb
@@ -0,0 +1,8 @@
+# This script enables puppet to manipulate traffic pools on a ZXTM Load Balancer
+# (c) Matthew Macdonald-Wallace 2011
+#
+# Include the relevant libraries
+require 'rubygems'
+require 'zxtm-api'
+
+
